b9f588653ac58a2721d7f396527a8dea31422e19
bleepbloop13 <rbak9218@740s-pl-998.local>
2016-04-22 13:19:37 -0600

    Draws rectangles of varying colors and thickness

--- src/graphics/view/DrawingPanel.java
+++ src/graphics/view/DrawingPanel.java
@@ -12,6 +12,7 @@ import javax.swing.*;
 public class DrawingPanel extends JPanel
 {
 	private GraphicsController baseController;
+	private ShapePanel shapePanel;
 	private SpringLayout baseLayout;
 	private JButton drawButton;
 	private ArrayList<Rectangle> rectangleList;
@@ -21,6 +22,9 @@ public class DrawingPanel extends JPanel
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
 		drawButton = new JButton("This adds rectangles");
+		rectangleList = new ArrayList<Rectangle>();
+		shapePanel = new ShapePanel();
+
 
 		setupPanel();
 		setupLayout();
@@ -32,21 +36,33 @@ public class DrawingPanel extends JPanel
 		this.setLayout(baseLayout);
 		this.setBackground(Color.BLACK);
 		this.add(drawButton);
+		this.add(shapePanel);
 	}
 
 	private void setupLayout()
 	{
 		baseLayout.putConstraint(SpringLayout.WEST, drawButton, 153, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.SOUTH, drawButton, -10, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, shapePanel, 50, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, shapePanel, 50, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, shapePanel, -50, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, shapePanel, -50, SpringLayout.EAST, this);
 	}
 
 	private void setupListeners()
 	{
 		drawButton.addActionListener(new ActionListener()
 		{
-			public void actionPerformed(ActionEvent onClick)
+			public void actionPerformed(ActionEvent click)
 			{
+				int xPosition = (int)(Math.random() * getWidth());
+				int yPosition = (int)(Math.random() * getHeight());
+				int width = (int)(Math.random() * 100);
+				int height = (int)(Math.random() * 100);
 				
+				rectangleList.add(new Rectangle(xPosition, yPosition, width, height));
+				
+				repaint();
 			}
 		});
 	}
@@ -68,8 +84,10 @@ public class DrawingPanel extends JPanel
 			int red = (int)(Math.random() * 256);
 			int blue = (int)(Math.random() * 256);
 			int green = (int)(Math.random() * 256);
+			
+			mainGraphics.setStroke(new BasicStroke((int)(Math.random() * 30)));
 			mainGraphics.setColor(new Color(red,green,blue));
-			mainGraphics.fill(current);
+			mainGraphics.draw(current);
 		}
 	}
 
--- src/graphics/view/ShapePanel.java
+++ src/graphics/view/ShapePanel.java
@@ -1,6 +1,41 @@
 package graphics.view;
 
-public class ShapePanel
-{
+import java.util.ArrayList;
+import java.awt.*;
+import java.awt.geom.*;
 
+import javax.swing.JPanel;
+
+public class ShapePanel extends JPanel
+{
+	private ArrayList<Rectangle> rectList;
+	private ArrayList<Ellipse2D> ellipseList;
+	private ArrayList<Ellipse2D> circleList;
+	private ArrayList<Polygon> triangleList;
+	private ArrayList<Polygon> polygonList;
+	
+	
+	public void addRectangle()
+	{
+		
+	}
+	
+	public void addSquare()
+	{
+		
+	}
+	
+	public void addEllipse()
+	{
+		
+	}
+
+	@Override
+	protected void paintComponent(Graphics currentGraphics)
+	{
+		super.paintComponent(currentGraphics);
+		
+		
+	}
+	
 }
